8a124c07c9cdfd1bc6cfe1377c11d6b571ad1f25
MARMOT <63745881+Swistek92@users.noreply.github.com>
2023-03-18 13:17:35 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            while (a == 1)
+            {
+                Console.WriteLine("hello world");
+                
+            }
+        }
+    }
+}
